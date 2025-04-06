adc5ec4419384a6d6c2d33e75d90ebb2f10a9e40
dindarosalin <dindarosalin24@gmail.com>
2025-04-06 19:37:52 +0700

    saw done

--- resources/views/livewire/projects/tasks/priorities.blade.php
+++ resources/views/livewire/projects/tasks/priorities.blade.php
@@ -1,5 +1,22 @@
 <div>
     <div class="card">
-        rekomendasi tugas
+    <table class="table table-sm table-bordered table-hover text-center" >
+                <thead>
+                    <tr>
+                        <th>Task Id</th>
+                        <th>Name</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($taskScored as $r)
+                        <tr>
+                            <td>{{$r->task_id}}</td>
+                            <td>{{$r->task_name}}</td>
+                            <td>{{$r->score}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
     </div>
 </div>
