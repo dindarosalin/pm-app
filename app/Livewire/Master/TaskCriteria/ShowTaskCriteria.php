e353878b63fd0417f731d1f82ae0eaf64144c619
dindarosalin <dindarosalin24@gmail.com>
2025-04-06 10:16:19 +0700

    add criteria master

--- app/Livewire/Master/TaskCriteria/ShowTaskCriteria.php
+++ app/Livewire/Master/TaskCriteria/ShowTaskCriteria.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Master\TaskCriteria;
 
 use App\Models\Projects\Master\TaskCriterias;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ShowTaskCriteria extends Component
